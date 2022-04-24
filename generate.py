104bf521287e5a59fd18e2b11470b83d653ea9c0
Ross <49296963+diceyecid@users.noreply.github.com>
2022-04-24 15:08:16 -0700

    added pixelization only

--- generate.py
+++ generate.py
@@ -23,31 +23,34 @@ def getArguments():
     parser = argparse.ArgumentParser( description = 'Generate parallax background' )
 
     parser.add_argument( '-i', '--input', type = str, required = True,
-            help = 'the image or directory with images to be used (this is a required argument)' )
+            help =  'the image or directory with images to be used (this is a required argument)' )
 
     parser.add_argument( '-o', '--output_dir', type = str, default = './output',
-            help = 'the directory where resulting images are to be saved' )
+            help =  'the directory where resulting images are to be saved' )
 
     parser.add_argument( '--output_width_factor', type = float, default = 4,
-            help = 'the scale factor to determine the width of resulting image' )
+            help =  'the scale factor to determine the width of resulting image' )
     
     parser.add_argument( '--output_height_factor', type = float, default = 1,
-            help = 'the scale factor to determine the height of resulting image' )
+            help =  'the scale factor to determine the height of resulting image' )
     
     parser.add_argument( '--output_width', type = int,
-            help = 'the width of resulting image (setting this will ignore \'output_width_factor\')' )
+            help =  'the width of resulting image (setting this will ignore \'output_width_factor\')' )
     
     parser.add_argument( '--output_height', type = int,
-            help = 'the height of resulting image (setting this will ignore \'output_height_factor\')' )
+            help =  'the height of resulting image (setting this will ignore \'output_height_factor\')' )
     
     parser.add_argument( '--n_colors', type = int, default = 8,
-            help = 'the number of dominate colours to be extracted as a palette for the image(s)' )
+            help =  'the number of dominate colours to be extracted as a palette for the image(s)' )
 
     parser.add_argument( '--recolor', action = 'store_true', default = False, 
-            help = 'the option to turn on palette recolouring for the image(s)' )
+            help =  'the option to turn on palette recolouring for the image(s)' )
     
     parser.add_argument( '--superpixel_size', type = int, default = 3,
-            help = 'the size of a \'pixel\' after pixelization' )
+            help =  'the size of a \'pixel\' after pixelization' )
+
+    parser.add_argument( '--pixelization_only', action = 'store_true', default = False,
+            help =  'the option to run only pixelization and recolouring part' )
     
     parser.add_argument( '--direction', type = int, default = 0, choices = [ 0, 1 ],
             help =  'the direction of texture generation; ' +
@@ -131,29 +134,35 @@ def main():
     # pixelize images and save them to output directory
     progressBar = tqdm( zip( images, imageSubpaths ), desc = 'Generating pixelized background', total = len( images ) )
     for im, sp in progressBar:
-        # pixelize image (part 1)
-        result = pixelizeP1( im, args.n_colors, args.recolor, args.superpixel_size )
-        # print( result.shape )
-        # cv2.imshow( 'pixelizeP1', result )
-        # cv2.waitKey( 0 )
+        # pixelization only
+        if args.pixelization_only:
+            result = pixelize( im, args.n_colors, args.recolor, args.superpixel_size )
 
-        # texture generation
-        outputWidth, outputHeight = getOutputSize( im, args, downsized = True )
-        # print( GenDirection( args.direction ).name, args.patch_factor,
-                            # GenMethod( args.generation_mode ).name, outputHeight, outputWidth )
-        result = graphCut( result, GenDirection( args.direction ), args.patch_factor,
-                            GenMethod( args.generation_mode ), outputHeight, outputWidth )
-        # print( result.shape )
-        # cv2.imshow( 'graphCut', result )
-        # cv2.waitKey( 0 )
+        # pixelization and generation
+        else:
+            # pixelize image (part 1)
+            result = pixelizeP1( im, args.n_colors, args.recolor, args.superpixel_size )
+            # print( result.shape )
+            # cv2.imshow( 'pixelizeP1', result )
+            # cv2.waitKey( 0 )
 
-        # pixelize image (part 2)
-        outputWidth, outputHeight = getOutputSize( im, args, downsized = False )
-        result = pixelizeP2( result, outputWidth, outputHeight )
-        # print( result.shape )
-        # cv2.imshow( 'pixelizeP2', result )
-        # cv2.waitKey( 0 )
-        # cv2.destroyAllWindows()
+            # texture generation
+            outputWidth, outputHeight = getOutputSize( im, args, downsized = True )
+            # print( GenDirection( args.direction ).name, args.patch_factor,
+                                # GenMethod( args.generation_mode ).name, outputHeight, outputWidth )
+            result = graphCut( result, GenDirection( args.direction ), args.patch_factor,
+                                GenMethod( args.generation_mode ), outputHeight, outputWidth )
+            # print( result.shape )
+            # cv2.imshow( 'graphCut', result )
+            # cv2.waitKey( 0 )
+
+            # pixelize image (part 2)
+            outputWidth, outputHeight = getOutputSize( im, args, downsized = False )
+            result = pixelizeP2( result, outputWidth, outputHeight )
+            # print( result.shape )
+            # cv2.imshow( 'pixelizeP2', result )
+            # cv2.waitKey( 0 )
+            # cv2.destroyAllWindows()
 
         # create subdirectories to match input directory structure if necessary
         saveDir = os.path.join( args.output_dir, os.path.dirname( sp ) )
