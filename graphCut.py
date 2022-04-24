44e573b0d6c5e1452d5751491bae6b677e29224e
OkaMoez <43766412+OkaMoez@users.noreply.github.com>
2022-04-23 19:37:25 -0700

    Fixes output size bug

--- graphCut.py
+++ graphCut.py
@@ -2,32 +2,31 @@
 import sys
 import time
 import numpy as np
+import cv2
 
 from graph import Graph
-from custom_io import read_img
-from custom_io import increment_status_message
-from cv2 import imwrite
-
-from graphEnums import GenDirection as GenDirection
-from graphEnums import GenMethod as GenMethod
+from custom_io import debug_out, increment_status_message
+from graphEnums import GenDirection, GenMethod
 
 def main():
     path_in = sys.argv[1] if len(sys.argv) > 1 else quit()
     path_out = sys.argv[2] if len(sys.argv) > 2 else "./"
-    pattern = read_img(path_in).astype(np.int32)
+    pattern = cv2.imread(path_in)
     h, w = pattern.shape[:2]
 
     direction = GenDirection(int(sys.argv[3])) if len(sys.argv) > 3 else GenDirection.HORIZONAL
     patchFactor =  int(sys.argv[4]) if len(sys.argv) > 4 else 8
     mode = GenMethod(int(sys.argv[5]) if len(sys.argv) > 5 else 2)
 
-    target_h = int(sys.argv[6]) if len(sys.argv) > 2 else int(2*h)
-    target_w = int(sys.argv[7]) if len(sys.argv) > 3 else int(2*w)
+    target_h = int(sys.argv[6]) if len(sys.argv) > 6 else int(2*h)
+    target_w = int(sys.argv[7]) if len(sys.argv) > 7 else int(2*w)
+
+    debug_out("path in: %s, path out: %s, direction: %s, factor: %i, mode: %s, height: %i, width: %i\n", path_in, path_out, direction.name, patchFactor, mode.name, target_h, target_w)
 
     image_out = graphCut( pattern, direction, patchFactor, mode, target_h, target_w)
-    imwrite(path_out, image_out)
+    cv2.imwrite(path_out, image_out)
 
-def graphCut( image_in, direction = GenDirection.HORIZONAL, patchFactor = 8, mode = GenMethod.SUBBLOCK_ROW, h_in = 0, w_in = 0):
+def graphCut( image_in, direction = GenDirection.HORIZONAL, patchFactor = 8, mode = GenMethod.SUBBLOCK_ROW, h_out = 0, w_out = 0):
     # def graphCut(inputImage.int32 direction patchFactor mode[1-3] outputWidth outputHeight):
     # parameters:
     # inputImage -> cv2.imread(path)
@@ -37,7 +36,7 @@ def graphCut( image_in, direction = GenDirection.HORIZONAL, patchFactor = 8, mod
     # mode -> int value from 1 to 3, using the starter code
     # outputWidth, outputHeight -> outputHeight equals inputHeight for horizontal generation
 
-    if (h_in is 0) and (w_in is 0):
+    if (h_out is 0) and (w_out is 0):
         return image_in
 
     image_in = image_in.astype(np.int32)
@@ -46,9 +45,10 @@ def graphCut( image_in, direction = GenDirection.HORIZONAL, patchFactor = 8, mod
     PATCH_W_RATIO = patchFactor
     sub_patch_size = (h_in//PATCH_H_RATIO, w_in//PATCH_W_RATIO)
 
-    g = Graph(h_in, w_in)
+    g = Graph(h_out, w_out)
     max = g.w*g.h
     counter = 0
+    debug_out("channels in: %i, channels out: %i\n", image_in.shape[2], g.canvas.shape[2])
 
     # Generate Texture in Random Order using Sub Patches
     if mode is GenMethod.SUBBLOCK_RANDOM:
